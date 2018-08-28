79d78b178654e4154c398da8c45e7ad9113a76d2
Dorian Lannay <dorian.lannay@outlook.fr>
2018-08-28 21:57:50 +0200

    PurposeUiKit Templating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { ValuesComponent } from './values/values.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { PricingsComponent } from './pricings/pricings.component';
+import { TeamsComponent } from './teams/teams.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { CtaComponent } from './cta/cta.component';
+import { BlogComponent } from './blog/blog.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    ValuesComponent,
+    TestimonialsComponent,
+    PricingsComponent,
+    TeamsComponent,
+    LoginComponent,
+    RegisterComponent,
+    CtaComponent,
+    BlogComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pricings/pricings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricings',
+  templateUrl: './pricings.component.html',
+  styleUrls: ['./pricings.component.css']
+})
+export class PricingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
