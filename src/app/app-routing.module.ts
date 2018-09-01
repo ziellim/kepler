23d79e5f638192aaa7fda22af3c5f777115e37a9
Dorian Lannay <dorian.lannay@outlook.fr>
2018-09-01 16:03:34 +0200

    Routing for Main - Login - Register - Pricings

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { MainComponent } from './main/main.component';
+import { PricingsComponent } from './pricings/pricings.component';
+import { BlogComponent } from './blog/blog.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'pricing', component: PricingsComponent },
+  { path: 'blog', component: BlogComponent },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [ RouterModule ],
+  declarations: []
+})
+export class AppRoutingModule { }
