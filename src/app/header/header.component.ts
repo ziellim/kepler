6fb5c4baf24555616ef6ede6594ebefa35e6de3e
Zied Guellim <guellim.z@sfeir.com>
2018-09-02 13:50:59 +0200

    switch css on header & footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit() {
+  }
+  isDark(){
+    const routesWithDarkCss = ['/home'];
+    return routesWithDarkCss.includes(this._router.url);
   }
 
 }
